3649b44f24d9a251ee5b41374ef09108310e48cd
Marcin Henzel <35427502+Lokomolo@users.noreply.github.com>
2018-03-14 13:09:30 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char x = (char)U00A2; /*Console.ReadKey().KeyChar;*/
+            //Console.WriteLine(x);
+            if (x == 224) { Console.WriteLine(y); }
+            Console.ReadKey();
+
+        }
+    }
+}
